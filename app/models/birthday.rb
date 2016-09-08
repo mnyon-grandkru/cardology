d8c4687b19b4332f5349e8af6e9cead419164dba
Mike Schwab <mike.schwab@gmail.com>
2016-09-08 19:05:15 -0400

    birth card calculation

--- app/models/birthday.rb
+++ app/models/birthday.rb
@@ -1,2 +1,7 @@
 class Birthday < ApplicationRecord
+  def birth_card
+    deck = Card.ordered_deck.unshift(Card.find_by(:face => Face.find_by(:name => 'Joker')))
+    position = month * 2 + day
+    deck.reverse[54 - position]
+  end
 end
