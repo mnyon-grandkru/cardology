f78232e39fe8de0da7fca1fe9f46195525d01b10
Mike Schwab <mike.schwab@gmail.com>
2016-09-08 21:10:13 -0400

    basic planetary card - needs edge cases

--- app/models/birthday.rb
+++ app/models/birthday.rb
@@ -13,10 +13,35 @@ class Birthday < ApplicationRecord
     ((Date.today - birthdate) / 365).floor
   end
   
+  def days_since_birthday
+    today = Date.today
+    if today.month > month
+      last_birthday = Date.new today.year, month, day
+    elsif today.month < month 
+      last_birthday = Date.new(today.year - 1, month, day)
+    elsif today.day < day
+      last_birthday = Date.new(today.year - 1, month, day)
+    else
+      last_birthday = Date.new today.year, month, day
+    end
+    
+    (today - last_birthday).floor
+  end
+  
   def card_for_this_year
     long_range_spread_index = age / 7
     planetary_position = (age % 7) + 1
     spread = Spread.find_by(:age => long_range_spread_index)
+    position_of_birth_card = spread.position_of birth_card
+    position = position_of_birth_card.position - planetary_position
+    position = 52 + position if position < 0
+    place = Place.find_by :spread_id => spread.id, :position => position
+    place.card
+  end
+  
+  def card_for_this_52_day_period
+    spread = Spread.find_by(:age => age)
+    planetary_position = (days_since_birthday / 52) + 1
     position_of_birth_card = spread.position_of birth_card
     position = position_of_birth_card.position - planetary_position
     position = 52 + position if position < 0
