1d2f74b895bb6f30d2aeb586275f9a385c3476c0
Mike Schwab <mike.schwab@gmail.com>
2017-01-26 18:36:44 -0500

    quotes better formatted

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -3,7 +3,7 @@ module QuotesHelper
     q = Quote.random
     content_tag :div, :class => 'quote_single' do
       [
-        content_tag(:span, q.phrasing, :class => 'poignant_quote'),
+        content_tag(:span, "“#{q.phrasing}”", :class => 'poignant_quote'),
         content_tag(:span, q.source, :class => 'poignant_person')
       ].join.html_safe
     end
