332a9129e318bcbb7bcc3052eea97017a3c5f653
Mike Schwab <mike.schwab@gmail.com>
2017-01-26 23:43:28 -0500

    open sans condensed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,8 @@ module ApplicationHelper
   def mark_up content
     markdown.render(content.to_s).html_safe
   end
+  
+  def google_fonts
+    content_tag :link, nil, :href => 'https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300', :rel => 'stylesheet'
+  end
 end
