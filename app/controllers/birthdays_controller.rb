91a21458a60bb4c0fbea6d355b7a6c4935848700
Mike Schwab <mike.schwab@gmail.com>
2017-04-24 15:41:11 -0400

    removed unused code from birthday display

--- app/controllers/birthdays_controller.rb
+++ app/controllers/birthdays_controller.rb
@@ -12,35 +12,35 @@ class BirthdaysController < ApplicationController
     @personality_card = @birthday.personality_card
     @yearly_card = @birthday.card_for_this_year
     @planetary_card = @birthday.card_for_this_52_day_period
-    @birth_card_explanation = @birth_card.interpretations.where(:reading => :birth).last&.explanation || Interpretation.last&.explanation
-    @personality_card_explanation = @personality_card.interpretations.where(:reading => :birth).last&.explanation || Interpretation.last&.explanation
-    @yearly_card_explanation = @yearly_card.interpretations.where(:reading => :yearly).last&.explanation || Interpretation.last&.explanation
-    @planetary_card_explanation = @planetary_card.interpretations.where(:reading => :yearly).last&.explanation || Interpretation.last&.explanation
+    @birth_card_explanation = @birth_card.interpretations.where(:reading => :birth).last&.explanation
+    @personality_card_explanation = @personality_card.interpretations.where(:reading => :birth).last&.explanation
+    @yearly_card_explanation = @yearly_card.interpretations.where(:reading => :yearly).last&.explanation
+    @planetary_card_explanation = @planetary_card.interpretations.where(:reading => :yearly).last&.explanation
   end
   
   def last_year
     @birthday = Birthday.find params[:id]
     @yearly_card = @birthday.card_for_last_year
-    @yearly_card_explanation = @yearly_card.interpretations.where(:reading => :yearly).last&.explanation || Interpretation.last&.explanation
+    @yearly_card_explanation = @yearly_card.interpretations.where(:reading => :yearly).last&.explanation
   end
   
   def this_year
     @birthday = Birthday.find params[:id]
     @yearly_card = @birthday.card_for_this_year
-    @yearly_card_explanation = @yearly_card.interpretations.where(:reading => :yearly).last&.explanation || Interpretation.last&.explanation
+    @yearly_card_explanation = @yearly_card.interpretations.where(:reading => :yearly).last&.explanation
     render :template => 'birthdays/last_year.js.erb'
   end
   
   def last_planet
     @birthday = Birthday.find params[:id]
     @planetary_card = @birthday.card_for_last_planet
-    @planetary_card_explanation = @planetary_card.interpretations.where(:reading => :yearly).last&.explanation || Interpretation.last&.explanation
+    @planetary_card_explanation = @planetary_card.interpretations.where(:reading => :yearly).last&.explanation
   end
   
   def this_planet
     @birthday = Birthday.find params[:id]
     @planetary_card = @birthday.card_for_this_52_day_period
-    @planetary_card_explanation = @planetary_card.interpretations.where(:reading => :yearly).last&.explanation || Interpretation.last&.explanation
+    @planetary_card_explanation = @planetary_card.interpretations.where(:reading => :yearly).last&.explanation
     render :template => 'birthdays/last_planet.js.erb'
   end
   
@@ -48,9 +48,8 @@ class BirthdaysController < ApplicationController
     @birthday = Birthday.find_or_create_by :year => birthday_params['date(1i)'], :month => birthday_params['date(2i)'], :day => birthday_params['date(3i)']
     @lookup = Lookup.create :birthday => @birthday, :ip_address => request.remote_ip
     @birth_card = @birthday.birth_card
-    @birth_card_explanation = @birth_card.interpretations.where(:reading => :birth).last&.explanation || Interpretation.last&.explanation
+    @birth_card_explanation = @birth_card.interpretations.where(:reading => :birth).last&.explanation
     render :template => 'birthdays/member_form'
-    # redirect_to @birthday
   end
   
   def birthday_params
