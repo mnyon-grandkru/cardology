7de82eb51ec68b710f1a66e40fc70bb9f512e15e
Mike Schwab <mike.schwab@gmail.com>
2017-05-03 11:25:42 -0400

    reset password messaging and redirect

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,3 +1,7 @@
 class PasswordsController < Devise::PasswordsController
   skip_before_action :verify_authenticity_token, :if => lambda { request.domain == 'herokuapp.com' }
+  
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    root_url :message => "Your email will arrive momentarily."
+  end
 end
