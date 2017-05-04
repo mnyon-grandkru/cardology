fe617e17bb18bab47d4c0d2cd752ccc213fcc3f3
Mike Schwab <mike.schwab@gmail.com>
2017-05-03 21:23:43 -0400

    user who signs up with existing email gets logged in

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,6 +2,12 @@ class MembersController < ApplicationController
   skip_before_action :verify_authenticity_token, :if => lambda { request.domain == 'herokuapp.com' }
 
   def create
+    if @member = Member.find_by_email(member_params[:email])
+      sign_in @member
+      redirect_to root_url
+      return
+    end
+    
     @member = Member.new member_params
     @member.password = Member.generate_password
     @member.password_confirmation = @member.password
