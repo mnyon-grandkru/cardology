66ecb04a6fc0e174434532d17d20b4e264bf00c4
Mike Schwab <mike.schwab@gmail.com>
2017-05-15 20:55:10 -0400

    line breaks in password explanation

--- app/controllers/birthdays_controller.rb
+++ app/controllers/birthdays_controller.rb
@@ -92,7 +92,7 @@ class BirthdaysController < ApplicationController
     @goal = 'Find Out Your Birth Card'
     @instructions = 'Enter your Date of Birth:'
     @email = params[:member][:email]
-    @password_explanation = "Oops, it looks like you're entering the wrong password. The temporary password was emailed to you when you signed up. Please check your email for the password."
+    @password_explanation = "Oops, it looks like you're entering the wrong password.<br>The temporary password was emailed to you when you signed up.<br>Please check your email for the password.".html_safe
     render 'new.html.erb'
   end
   
