3670e1ef1c03716ebaf38bb765b4190dedd965f3
Mike Schwab <mike.schwab@gmail.com>
2017-06-28 19:47:07 -0400

    use multiple client tokens to have 3 forms

--- app/helpers/braintree_helper.rb
+++ app/helpers/braintree_helper.rb
@@ -4,7 +4,7 @@ module BraintreeHelper
       content_tag(:div, :class => 'braintree_container') do
         braintree_dropin +
         form_with(:url => subscriptions_path, :class => "subscription_payment") do |subscription_form|
-          tag.div(:class => 'bt-dropin') +
+          tag.div(:id => "bt-dropin-#{dropin_count}") +
           subscription_form.hidden_field(:payment_method_nonce, :class => 'nonce') +
           subscription_form.hidden_field(:member_id, :value => @member.id) +
           subscription_form.submit(marketing_text('subscription', 'checkout', 'submit'), :id => 'subscribe_button')
@@ -15,6 +15,10 @@ module BraintreeHelper
   
   def braintree_dropin
     tag.script :src => "https://js.braintreegateway.com/web/dropin/1.2.0/js/dropin.min.js"
+  end
+  
+  def dropin_count
+    @dropin_count = @dropin_count.to_i + 1
   end
   
   def braintree_enabled?
