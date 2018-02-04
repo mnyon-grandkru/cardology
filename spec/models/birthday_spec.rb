de41ac6daa3571c12e3f1e3ef31e94b9d58e5ece
Mike Schwab <mike.schwab@gmail.com>
2018-02-04 15:32:03 -0500

    test for planetary card

--- spec/models/birthday_spec.rb
+++ spec/models/birthday_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Birthday, type: :model do
     @birthday = Fabricate :birthday, :month => 4
     expect(@birthday.name).to include('April')
   end
+  
+  describe "planetary card" do
+    it "for current planet" do
+      @birthday = Fabricate :birthday, :year => 1980, :month => 8, :day => 20
+      travel_to Time.zone.local(2018, 2, 4, 1, 0, 0)
+      @planetary_card = @birthday.card_for_this_planet
+      expect(@planetary_card.name).to include('Two of Spades')
+    end
+  end
 end
