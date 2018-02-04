9ae688dc661fb3a21b7ffde1ef802623da7ebd7a
Mike Schwab <mike.schwab@gmail.com>
2018-02-04 15:33:59 -0500

    spec for approaching planet

--- spec/models/birthday_spec.rb
+++ spec/models/birthday_spec.rb
@@ -7,11 +7,23 @@ RSpec.describe Birthday, type: :model do
   end
   
   describe "planetary card" do
-    it "for current planet" do
+    before do
       @birthday = Fabricate :birthday, :year => 1980, :month => 8, :day => 20
       travel_to Time.zone.local(2018, 2, 4, 1, 0, 0)
+    end
+    
+    after do
+      travel_back
+    end
+    
+    it "for current planet" do
       @planetary_card = @birthday.card_for_this_planet
       expect(@planetary_card.name).to include('Two of Spades')
+    end
+    
+    it "for approaching planet" do
+      @planetary_card = @birthday.card_for_next_planet
+      expect(@planetary_card.name).to include('Ten of Diamonds')
     end
   end
 end
