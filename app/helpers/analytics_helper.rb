8d8059628bcccc46f649278d293ad3631516d4b4
Mike Schwab <mike.schwab@gmail.com>
2018-04-16 14:39:54 -0400

    google analytics

---
+++ app/helpers/analytics_helper.rb
@@ -0,0 +1,14 @@
+module AnalyticsHelper
+  def google_analytics
+    javascript_include_tag("https://www.googletagmanager.com/gtag/js?id=#{ENV['GOOGLE_ANALYTICS_URCHIN']}", :async => true) +
+    javascript_tag do <<-SCRIPT
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      
+      gtag('config', '#{ENV['GOOGLE_ANALYTICS_URCHIN']}');
+      SCRIPT
+      .html_safe
+    end
+  end
+end
