2226bf5ed0f8de0467b656ffe29fa86c7405e1d4
Mike Schwab <mike.schwab@gmail.com>
2018-07-18 16:03:49 -0400

    Revert "same call to action for all four panes"
    This reverts commit afabc8535f68dfd8066c195934f5b4029038b761.

--- app/helpers/readings_helper.rb
+++ app/helpers/readings_helper.rb
@@ -13,7 +13,11 @@ module ReadingsHelper
             end +
             image_tag(card.image, :class => 'card_face_image') +
             div_for(Card.last, :opportunities_on) do
-              "#{marketing_text 'book', 'question'} #{link_to(marketing_text('book', 'answer'), ENV['BOOK_PURCHASE_LINK'], :target => '_blank')}".html_safe
+              if reading == :yearly
+                "#{marketing_text 'coaching', 'question'} #{link_to(marketing_text('coaching', 'answer'), ENV['COACHING_PURCHASE_LINK'], :target => '_blank')}".html_safe
+              else
+                "#{marketing_text 'book', 'question'} #{link_to(marketing_text('book', 'answer'), ENV['BOOK_PURCHASE_LINK'], :target => '_blank')}".html_safe
+              end
             end
           end +
           div_for(card, :explication_of) do
