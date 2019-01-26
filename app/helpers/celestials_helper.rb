7feee1a44c7dde4ea679b01b350a82409f560034
Mike Schwab <mike.schwab@gmail.com>
2019-01-26 18:28:30 -0500

    celestials can be removed

--- app/helpers/celestials_helper.rb
+++ app/helpers/celestials_helper.rb
@@ -1,2 +1,9 @@
 module CelestialsHelper
+  def celestial_button_for celestial, shown = nil
+    if celestial.birthday == shown
+      link_to marketing_text('preserving', 'remove') + ' ' + celestial.identifier, celestial, :method => :delete, :class => 'lunar_navigation axis', :title => marketing_text('preserving', 'remove')
+    else
+      link_to celestial.identifier, birthday_path(celestial.birthday, :navigation_shown => true), :class => 'lunar_navigation'
+    end
+  end
 end
