710d218f504ba6d1714b65cc48a90d953c4a0a66
ankush_gupta <ankushgupta70@gmail.com>
2021-07-25 01:07:27 +0530

    fix: css and other issue

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,15 +1,15 @@
-SecureHeaders::Configuration.default do |config|
-  config.csp = {
-    default_src: %w('*'),
-    script_src: SecureHeaders::OPT_OUT,
-    frame_src: %w(https://lifeelevated.life https://thesourcecards.com)
-
-    # connect_src: %w('self'),
-    # img_src: %w(cdn.example.com data:),
-    # font_src: %w(cdn.example.com data:),
-    # base_uri: %w('self'),
-    # style_src: %w('unsafe-inline' cdn.example.com),
-    # form_action: %w('self'),
-    # report_uri: %w(/mgmt/csp_reports)
-  }
-end
+# SecureHeaders::Configuration.default do |config|
+#   config.csp = {
+#     default_src: %w('*'),
+#     script_src: SecureHeaders::OPT_OUT,
+#     frame_src: %w(https://lifeelevated.life https://thesourcecards.com),
+#     style_src: %w('unsafe-inline' 'self'),
+#     # connect_src: %w('self'),
+#     # img_src: %w(cdn.example.com data:),
+#     # font_src: %w(cdn.example.com data:),
+#     # base_uri: %w('self'),
+#     # style_src: %w('unsafe-inline' cdn.example.com),
+#     # form_action: %w('self'),
+#     # report_uri: %w(/mgmt/csp_reports)
+#   }
+# end
