155baad91908e2c11ff0d05f6b68baf17761b7e2
ankush_gupta <ankushgupta70@gmail.com>
2021-07-24 12:49:19 +0530

    fix: clear header

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,10 @@ module Cardology
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.action_dispatch.default_headers["X-Content-Security-Policy"] = "FRAME-ANCESTORS https://*.lifeelevated.life";
-    config.action_dispatch.default_headers["Content-Security-Policy"] = "FRAME-ANCESTORS https://*.lifeelevated.life";
-    config.action_dispatch.default_headers["X-Frame-Options"] = "FRAME-ANCESTORS https://www.lifeelevated.life/";
-    config.action_dispatch.default_headers["Access-Control-Allow-Origin"] = "*";
+    # config.action_dispatch.default_headers["X-Content-Security-Policy"] = "FRAME-ANCESTORS https://*.lifeelevated.life";
+    # config.action_dispatch.default_headers["Content-Security-Policy"] = "FRAME-ANCESTORS https://*.lifeelevated.life";
+    # config.action_dispatch.default_headers["X-Frame-Options"] = "FRAME-ANCESTORS https://www.lifeelevated.life/";
+    # config.action_dispatch.default_headers["Access-Control-Allow-Origin"] = "*";
+    config.action_dispatch.default_headers.clear
   end
 end
