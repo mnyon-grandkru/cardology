90128b2ef111ea68a436a76608127d50eade34f4
Mike Schwab <mike.schwab@gmail.com>
2021-10-26 18:33:00 -0400

    spot to select birthday for 'deliveries' side of application

---
+++ app/controllers/deliveries_controller.rb
@@ -0,0 +1,5 @@
+class DeliveriesController < ApplicationController
+  def entrance
+    @date = DateTime.now
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :deliveries do
+    collection do
+      get :entrance
+    end
+  end
+  
   resources :interviews do
     collection do
       get :review
