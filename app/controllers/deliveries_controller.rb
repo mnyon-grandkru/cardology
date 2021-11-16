9215506220a8d5b3c21320b4555b2dbfddd785cf
Mike Schwab <mike.schwab@gmail.com>
2021-11-15 19:36:30 -0500

    birthday cards plugged in for January

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -7,5 +7,6 @@ class DeliveriesController < ApplicationController
     @birthday = Birthday.find params[:id]
     @birth_card = @birthday.birth_card
     @personality_card = @birthday.personality_card
+    @birthday_card = YAML.load(File.read(Rails.root.join('config', 'birthday_cards.yml')))[@birthday.month]&.[] @birthday.day
   end
 end
