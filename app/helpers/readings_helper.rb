5fbc38b0ffc3cbf34f2fa4d36a0a6b8dacaa9417
rorong <rordev@ongraph.com>
2022-11-16 18:50:45 +0530

    Added flip Card Functionality to the guidances/show

--- app/helpers/readings_helper.rb
+++ app/helpers/readings_helper.rb
@@ -70,4 +70,27 @@ module ReadingsHelper
   def quick_zodiac_picker birthday
     
   end
+  def card_reading_pane_gui card, reading, title, subtitle
+    
+    div_for @birthday, "#{reading}_card_for", :class => "card_reading_pane_gui pane", style: "width: 320px;" do
+      content_tag(:header, title) +
+      content_tag(:header, subtitle, :class => 'subtitle') +
+      if card
+        div_for(card, :identification_of) do
+          div_for(card, :name_of) do
+            card.name
+          end +
+          image_tag(card.image, :class => 'card_face_image_gui', style:"width:125px") +
+          div_for(Card.last, :opportunities_on) do
+           
+          end
+        end +
+        div_for(card, :explication_of) do
+          mark_up interpretation_of(card, reading)
+        end
+      end 
+      
+    end
+  
+end
 end
