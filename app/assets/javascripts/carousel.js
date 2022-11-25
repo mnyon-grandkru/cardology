21487248e120abf7786d54950ec2ae6187f02618
rorong <rordev@ongraph.com>
2022-11-25 11:40:33 +0530

    Fixed carousel transition and UI bugs

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
         responsiveClass:true,
         nav:true,
         navText:["❰","❯"],
+        smartSpeed: 1400,
         responsive:{
             0:{
                 items:1
