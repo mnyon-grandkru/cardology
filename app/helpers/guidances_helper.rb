7690306161d78477964054ee72d83da5b8a02e6d
rorong <rordev@ongraph.com>
2022-11-29 16:52:36 +0530

    guidance show page ui fixes

--- app/helpers/guidances_helper.rb
+++ app/helpers/guidances_helper.rb
@@ -1,12 +1,17 @@
 module GuidancesHelper
     
     def text_carousel(arr)
-            split_arr = arr.split(".",3)    
+        
+            split_arr = arr.split(".",3) || split_arr = arr.split("?",3)
             value1 = split_arr[0]+". " +split_arr[1]+". " 
             value2 = split_arr[2]
             join = []
             join<<value1
-            join<<value2      
+            join<<value2 
+        
     end
+    
 end
+
+
 
