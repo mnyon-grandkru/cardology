6ba809c9607a4f9a8abce0878ace861cd6398015
rorong <rordev@ongraph.com>
2022-12-08 15:52:22 +0530

    Fixed some bugs and cleaned and formatted the code

--- app/helpers/guidances_helper.rb
+++ app/helpers/guidances_helper.rb
@@ -1,12 +1,12 @@
 module GuidancesHelper
-    
+
     def text_carousel(carousel_string)
-            split_string = carousel_string.split(".",3) || split_string = carousel_string.split("?",3)
-            value1 = split_string[0]+". " +split_string[1]+". " 
-            value2 = split_string[2]
-            join_strings = []
-            join_strings<<value1
-            join_strings<<value2 
+        split_string = carousel_string.split(".",3) 
+        value1 = split_string[0]+". " +split_string[1]+". " 
+        value2 = split_string[2]
+        join_strings = []
+        join_strings<<value1
+        join_strings<<value2 
     end
     
 end
