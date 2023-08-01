072815d07b8d191e054fd3cd76e181b12c6507b8
rorong <rordev@ongraph.com>
2023-08-01 14:56:03 +0530

    Fixed the intrepretations bugs where added paragraphs are missed

--- app/helpers/guidances_helper.rb
+++ app/helpers/guidances_helper.rb
@@ -1,14 +1,19 @@
 module GuidancesHelper
+	def split_carousel_text(carousel_text)
+		return [carousel_text] unless carousel_text.length > 450
+		midpoint = carousel_text.length / 2
+		split_point = carousel_text.index(' ', midpoint)
+		first_half = carousel_text[0...split_point].strip + " ............."
+		second_half = carousel_text[split_point..-1].strip
+		return [first_half, second_half]
+		end
 
-    def text_carousel(carousel_string)
-        split_string = carousel_string.split(".",3) 
-        value1 = split_string[0]+". " +split_string[1]+". " 
-        value2 = split_string[2]
-        join_strings = []
-        join_strings<<value1
-        join_strings<<value2 
-    end
-    
+	def show_carousel(carousel_text)
+		split_carousel_text(carousel_text).each do |text|
+				yield text unless text.strip.empty?
+		end
+	end
+
 end
 
 
