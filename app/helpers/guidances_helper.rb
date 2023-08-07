76793ff86b5e975c258949359f9bc08a8df473b6
rorong <rordev@ongraph.com>
2023-08-07 15:05:23 +0530

    Fixed UI ,  and paragraphs in Carousel

--- app/helpers/guidances_helper.rb
+++ app/helpers/guidances_helper.rb
@@ -1,16 +1,16 @@
 module GuidancesHelper
 	def split_carousel_text(carousel_text)
-		return [carousel_text] unless carousel_text.length > 450
+		return [carousel_text] unless carousel_text.length > 620
 		midpoint = carousel_text.length / 2
 		split_point = carousel_text.index(' ', midpoint)
-		first_half = carousel_text[0...split_point].strip + " ............."
+		first_half = carousel_text[0...split_point].strip 
 		second_half = carousel_text[split_point..-1].strip
 		return [first_half, second_half]
 		end
 
 	def show_carousel(carousel_text)
 		split_carousel_text(carousel_text).each do |text|
-				yield text unless text.strip.empty?
+			yield text unless text.strip.empty?
 		end
 	end
 
