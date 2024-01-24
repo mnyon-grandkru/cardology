08a57ebb7e02474940d441317d6794d4e5a60c8f
rorong <rordev@ongraph.com>
2024-01-24 11:08:09 +0530

    add domain name thecardsoflife

--- app/controllers/guidances_controller.rb
+++ app/controllers/guidances_controller.rb
@@ -1,5 +1,5 @@
 class GuidancesController < ApplicationController
-  skip_before_action :verify_authenticity_token, :if => lambda { ['lifeelevated.life', 'thesourcecards.com', 'herokuapp.com' , 'blueprint.thesourcecards.com'].include? request.domain }
+  skip_before_action :verify_authenticity_token, :if => lambda { ['lifeelevated.life', 'thesourcecards.com', 'herokuapp.com' , 'blueprint.thesourcecards.com', 'thecardsoflife.com'].include? request.domain }
 
   def prompt
     @date = rand((50.years.ago)..20.years.ago)
