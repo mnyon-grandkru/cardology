f4320ca7d0356c1c07914ff3b63bed935f6204dc
Mike Schwab <mike.schwab@gmail.com>
2024-10-02 16:38:36 -0400

    buttons turn card box

--- app/assets/javascripts/card_box.js
+++ app/assets/javascripts/card_box.js
@@ -11,8 +11,45 @@ $(document).on('turbolinks:load', function() {
     rotation += 90;
     panel.style.transform = `translateZ(-165px) rotateY(${rotation}deg)`;
   }
+  
+  function currentRotation() {
+    // use panelvar
+    const computedStyle = window.getComputedStyle(panel);
+    const transformMatrix = computedStyle.transform;
+    let degrees;
+    if (transformMatrix !== "none") {
+      console.log('---------');
+      console.log(transformMatrix);
+      console.log('#########');
+      const values = transformMatrix.split('(')[1].split(')')[0].split(',');
+      const cosine = values[0];
+      const sine = values[2];
+      const radians = Math.atan2(sine, cosine)
+      degrees = radians * (180 / Math.PI);
+      console.log("Y rotation: ${degrees} degrees");
+    } else {
+      degrees = 0
+      console.log("No Transform");
+    }
+    return degrees;
+  }
+  
+  function rotateForward() {
+    let currentPosition = currentRotation();
+    desiredPosition = currentPosition - 90;
+    panel.style.transform = `translateZ(-165px) rotateY(${desiredPosition}deg)`;
+  }
+  
+  function rotateBackward() {
+    let currentPosition = currentRotation();
+    desiredPosition = currentPosition + 90;
+    panel.style.transform = `translateZ(-165px) rotateY(${desiredPosition}deg)`;
+  }
+  
+  window.rotateForward = rotateForward;
+  window.rotateBackward = rotateBackward;
 
-  setInterval(rotateBox, 3000); // Rotate every 3 seconds
+  setInterval(rotateBox, 10000); // Rotate every 3 seconds
 });
 
 // Remove the duplicate DOMContentLoaded event listener
