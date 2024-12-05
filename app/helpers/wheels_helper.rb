aec99d41eab604cd8fa84d6a0b1915ab2d470b35
Mike Schwab <mike.schwab@gmail.com>
2024-12-05 15:10:01 -0500

    wheel rim titles

--- app/helpers/wheels_helper.rb
+++ app/helpers/wheels_helper.rb
@@ -1,6 +1,7 @@
 module WheelsHelper
   def wheel_rim cards, type
     content_tag :div, :class => "wheel-rim #{type}" do
+      content_tag(:header, type.to_s.titleize) +
       cards.map do |card|
         content_tag :div, :class => "wheel-rim-card" do
           image_tag card.image_url
